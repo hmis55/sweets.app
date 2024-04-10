8a2fd79ee852ca38c742a34a93e435ba7bc56d4a
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-04-10 06:24:39 +0000

    [add]　ヘッダーレイアウ

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,22 @@
 class Public::PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  
+  # 投稿データの保存
+  def create
+    @post = Post.new(post_image_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path
+  end
+  
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post).permit(:title, :body)
+  end
+  
 end
