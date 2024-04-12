8e0afa50364ab3d99ee994139a9173c271d533a9
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-04-12 17:30:58 +0000

    [Add]デリート機能、編集機能,

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -3,6 +3,7 @@ class Public::PostsController < ApplicationController
     @post = Post.new
   end
 
+
   # 投稿データの保存
   def create
     @post = Post.new(post_params)
@@ -17,11 +18,34 @@ class Public::PostsController < ApplicationController
    @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+  end
+
+  def edit
+     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id), notice: "Post was successfully updated."
+    else
+      render :post_path
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post_id = @post.id
+    @post.destroy
+    redirect_to posts_path
+  end
   # 投稿データのストロングパラメータ
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :image, :user_id)
   end
-
 end
