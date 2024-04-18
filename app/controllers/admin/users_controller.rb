5612e5362c5195c2a7370838b353fd766658aca6
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-04-18 05:34:09 +0000

    [[Add]管理者側、会員ステータス変更機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -28,7 +28,7 @@ before_action :authenticate_admin!
   private
 
   def user_params
-   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   params.require(:user).permit(:name, :email, :password, :password_confirmation, :is_active)
   end
 
   def post_params
