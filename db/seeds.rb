0c66ca366533529fab70b9c18b3d6e37dfc9d331
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-04-25 13:54:26 +0000

    レイアウト修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+山田太郎= User.find_or_create_by!(email: "ya@ya") do |user|
+  user.name = 山田太郎
+  user.password = "111111"
+end
+
+佐藤次郎= User.find_or_create_by!(email: "sa@sa") do |user|
+  user.name = 佐藤次郎
+  user.password = "222222"
+end
+
+鈴木一郎= User.find_or_create_by!(email: "su@su") do |user|
+  user.name = 鈴木一郎
+  user.password = "333333"
+end
+
+田中太郎= User.find_or_create_by!(email: "ta@ta") do |user|
+  user.name = 田中太郎
+  user.password = "444444"
+end
+
+吉田次郎= User.find_or_create_by!(email: "yo@yo") do |user|
+  user.name = 吉田次郎
+  user.password = "555555"
+end
